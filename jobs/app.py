cbe07aea9b2cf9a9efd8e15cc633214b68f5b1a3
José Roberto Araújo <jroberto.araujo@gmail.com>
2020-01-06 01:23:40 +0000

    fix add date variable

--- jobs/app.py
+++ jobs/app.py
@@ -64,8 +64,7 @@ def review(employer_id):
         rating = request.form['rating']
         status = request.form['status']
 
-        date = datetime.datetime.now().strftime("%m/%d/%y")
-
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
 
         return redirect(url_for('employer', employer_id=employer_id))
